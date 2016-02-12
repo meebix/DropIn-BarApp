50070aeaf09b38b669fd4f384d39926abc679b7a
Mike Donahue <mtdonahue1@gmail.com>
2016-02-11 23:50:10 -0800

    Completed mobile ready page data drop in analytics

--- src/client/app/core/core.module.js
+++ src/client/app/core/core.module.js
@@ -4,7 +4,7 @@
   angular
     .module('app.core', [
       'ngAnimate', 'ngSanitize', 'ngCookies',
-      'blocks.exception', 'blocks.logger', 'blocks.router', 'blocks.filters',
+      'blocks.exception', 'blocks.logger', 'blocks.router', 'blocks.filters', 'blocks.directives',
       'ui.router', 'ngplus', 'angularMoment', 'angularSpinner'
     ]);
 })();
